ab7534473f11c175d833b36ca7a0ae82c1bd4d4a
xiaosonggege <1243049371@qq.com>
2020-03-13 21:59:59 +0800

    y

--- No_118/generate.py
+++ No_118/generate.py
@@ -8,11 +8,34 @@
 @file: generate
 @time: 2020/3/13 5:26 下午
 '''
+import copy
+import numpy as np
 
-
-class Name:
-    pass
-
+class Generate:
+    def __init__(self, retrain:int):
+        self._retrain = retrain
+        self._flag = 0
+        self._row = [1]
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if not self._flag :
+            self._flag += 1
+            return self._row
+        else:
+            lis = np.array(copy.deepcopy(self._row) + [0])
+            self._row = (np.array([0] + self._row) + lis).tolist()
+            if self._flag == self._retrain:
+                raise StopIteration
+            self._flag += 1
+            return self._row
+    def __enter__(self):
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return True
 
 if __name__ == '__main__':
-    pass
+    with Generate(9) as g:
+        for i in g:
+            print(i)
+
---
+++ No_119/getRow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2020-2025.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: getRow
+@time: 2020/3/13 9:59 下午
+'''
+
+
+class GetRow:
+    pass
+
+
+if __name__ == '__main__':
+    pass
