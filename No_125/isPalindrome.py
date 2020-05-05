ef2068eea2f3950209d16cc34c8a387577e928a1
xiaosonggege <1243049371@qq.com>
2020-05-05 14:08:27 +0800

    y

--- No_125/isPalindrome.py
+++ No_125/isPalindrome.py
@@ -10,9 +10,50 @@
 '''
 
 
-class Name:
-    pass
+class IsPalinDrome:
+
+    @staticmethod
+    def range_judge(e:str):
+        if e.lower() >= 'a' and e.lower() <= 'z' or e >= '0' and e <= '9':
+            return True
+        else:
+            return False
+
+    def __init__(self, strs:str):
+        self._strs = strs
+
+    def judge(self, i:int, j:int):
+        # print(self._strs[i], self._strs[j])
+        #j索引位置不是可判断字符
+        if not IsPalinDrome.range_judge(self._strs[j]):
+            if j > i+1:
+                self.judge(i, j-1)
+            else:
+                print('True')
+        #i索引位置不是可判断字符
+        if not IsPalinDrome.range_judge(self._strs[i]):
+            if i < j-1:
+                self.judge(i+1, j)
+            else:
+                print('True')
+        #i和j位置的字符都是可以判断字符时
+        if IsPalinDrome.range_judge(self._strs[i]) and IsPalinDrome.range_judge(self._strs[j]):
+            if self._strs[i].lower() != self._strs[j].lower():
+                print('False')
+            elif j - i >= 3:  # 中间间隔两个
+                self.judge(i+1, j-1)
+            else:
+                print('True')
+
+    def __enter__(self):
+        self.judge(0, self._strs.__len__()-1)
+        return None
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
 if __name__ == '__main__':
-    pass
+    with IsPalinDrome('A man, a plan, a canal: Panama') as m:
+        pass
+    with IsPalinDrome('race a car') as n:
+        pass
