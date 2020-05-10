046d078384e985efd86c80355be1dacd14df174b
xiaosonggege <1243049371@qq.com>
2020-05-10 10:34:26 +0800

    y

--- No_169/majorityElement.py
+++ No_169/majorityElement.py
@@ -8,11 +8,49 @@
 @file: majorityElement
 @time: 2020/5/9 9:29 上午
 '''
+import copy
+class MajorityElement:
+    def __init__(self, nums:list):
+        self._nums = nums
+        self._target = None
+
+    def insert_sort(self)->list:
+        nums = copy.deepcopy(self._nums)
+        flag = nums[0]
+        for i in range(1, len(self._nums)):
+            flag = nums[i]
+            for j in range(i-1, -1, -1):
+                if nums[j] > flag:
+                    nums[j+1] = nums[j]
+                else:
+                    nums[j+1] = flag
+                    break
+                if j == 0:
+                    nums[0] = flag
+        return nums
+
+    def merger_sort_sub(self, nums1:list, nums2:list)->list:
+        result = []
+        while len(nums1) and len(nums2):
+            result.append(nums1.pop(0) if nums1[0] <= nums2[0] else nums2.pop(0))
+        result.extend(nums1 if len(nums1) else nums2)
+        return result
+
+    def merger_sort(self, nums:list)->list:
+        if len(nums) > 1:
+            nums = self.merger_sort_sub(nums1=nums[:len(nums)//2+1], nums2=nums[len(nums)//2+1:len(nums)])
+        else:
 
 
-class Name:
-    pass
+
+
+
+    def mE(self):
+        return self.insert_sort()[self.insert_sort().__len__()//2]
 
 
 if __name__ == '__main__':
-    pass
+    m = MajorityElement(nums=[2, 1, 5, 3, 6, 4, 7, 2, 2, 2, 2, 2])
+    # print(m.insert_sort())
+    # print(m.mE())
+    print(m.merger_sort_sub(nums1=[1, 2, 3, 5], nums2=[2, 4, 6, 7]))
