003f2d8dfefdf0a5502852cf5d0ef3bdbc71469f
xiaosonggege <1243049371@qq.com>
2020-05-16 23:47:37 +0800

    y

--- No_172/trailingZeroes.py
+++ No_172/trailingZeroes.py
@@ -10,9 +10,47 @@
 '''
 
 
-class Name:
-    pass
+class TrailingZeroes:
+    def __init__(self, n:int)->None:
+        self._n = n
+        self._n5 = 0
+        self._n2 = 0
 
+    def testTwo(self, n:int)->None:
+        if n >= 2 and n % 2 == 0:
+            self._n2 += 1
+            self.testTwo(n//2)
+
+    def testFive(self, n:int)->None:
+        if n % 5 == 0:
+            self._n5 += 1
+            self.testFive(n // 5)
+        else:
+            self.testTwo(n)
+
+    def logn_function(self, n:int)->int:
+        if n:
+            return n // 5 + self.logn_function(n // 5)
+        else:
+            return 0
+
+    def __enter__(self):
+        # for n in range(self._n, 0, -1):
+        #     self.testFive(n)
+        self._n5 = self.logn_function(self._n)
+        return self._n5
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 if __name__ == '__main__':
-    pass
+    with TrailingZeroes(5) as result:
+        print(result)
+    with TrailingZeroes(6) as result:
+        print(result)
+    with TrailingZeroes(10) as result:
+        print(result)
+    with TrailingZeroes(15) as result:
+        print(result)
+    with TrailingZeroes(20) as result:
+        print(result)
