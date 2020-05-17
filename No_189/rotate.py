01f94680baef7b1abec0f0ded663e42468f0a4db
xiaosonggege <1243049371@qq.com>
2020-05-17 15:16:47 +0800

    y

---
+++ No_189/rotate.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2020-2025.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: rotate
+@time: 2020/5/16 11:54 下午
+'''
+import copy
+
+class Rotate:
+    def __init__(self, nums:list, k:int):
+        self._nums = nums
+        self._k = k
+    def rotate_func1(self, nums:list, k:int)->list:
+        nums = copy.deepcopy(nums)
+        l = len(nums)
+        time = 0
+        move = 0
+        while time < l-k:
+            temp = nums[0]
+            while move < l-1:
+                nums[move] = nums[move+1]
+                move += 1
+            nums[l-1] = temp
+            time += 1
+            move = 0
+        return nums
+
+    def rotate_func2(self, nums:list, k:int)->list:
+        nums = copy.deepcopy(nums)
+        l = len(nums)
+        time = 0
+        while time < k:
+            move = l - 1
+            temp = nums[l-1]
+            while move > 0:
+                nums[move] = nums[move - 1]
+                move -= 1
+            nums[0] = temp
+            time += 1
+        return nums
+
+    def rotate_func3(self, nums:list, k:int)->list:
+        nums = copy.deepcopy(nums)
+        move = 0
+        l = len(nums)
+        while move < k:
+            r = move + l - k
+            nums[move], nums[r] = nums[r], nums[move]
+            move += 1
+
+        move1 = move
+        time = 0
+        while time < l-k-move1:
+            move = move1
+            temp = nums[move1]
+            while move < l - 1:
+                nums[move] = nums[move + 1]
+                move += 1
+            nums[move] = temp
+            time += 1
+        return nums
+
+    def rotate_func4(self, nums:list, k:int)->list: #环形队列队头队尾指针移动o(n),o(1)
+        pass
+
+    def __enter__(self):
+        return self.rotate_func1(self._nums, self._k), self.rotate_func2(self._nums, self._k), \
+               self.rotate_func3(self._nums, self._k)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+if __name__ == '__main__':
+    with Rotate(nums=[1,2,3,4,5,6,7,8,9], k=3) as r:
+        print(r)
