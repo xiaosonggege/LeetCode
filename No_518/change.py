5d90a6754fcbc598bdb3d19e0412a668720d584c
xiaosonggege <1243049371@qq.com>
2020-05-25 22:20:13 +0800

    y

---
+++ No_518/change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2020-2025.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: change
+@time: 2020/5/25 10:19 下午
+'''
+
+class Change:
+    pass
+
+
+if __name__ == '__main__':
+    pass
