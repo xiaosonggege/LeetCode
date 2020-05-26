dfdaa3198bac4d95c17ff75ac57e896e1ff958f4
xiaosonggege <1243049371@qq.com>
2020-05-26 16:31:20 +0800

    y

--- No_518/change.py
+++ No_518/change.py
@@ -8,10 +8,25 @@
 @file: change
 @time: 2020/5/25 10:19 下午
 '''
-
+import copy
 class Change:
-    pass
+    def __init__(self, amount:int, coins:list):
+        self._amount = amount
+        self._coins = coins
+        self._combining_num = [1] + [0] * self._amount
+    def finding(self):
+        for coin in self._coins:
+            for i in range(coin, self._combining_num.__len__()):
+                self._combining_num[i] += self._combining_num[i-coin]
+        return self._combining_num[-1]
 
+    def __enter__(self):
+        result = self.finding()
+        return result
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 if __name__ == '__main__':
-    pass
+    with Change(amount=5, coins=[1, 2, 5]) as r:
+        print(r)
