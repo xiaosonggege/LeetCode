03b40fd86bc4a9bfcc87f980d2c74276d4ef193b
xiaosonggege <1243049371@qq.com>
2020-06-07 15:07:56 +0800

    y

---
+++ No_11/maxArea.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2020-2025.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: No_11
+@time: 2020/6/7 11:36 上午
+'''
+
+class MaxArea:
+    pass
+
+
+if __name__ == '__main__':
+    pass
