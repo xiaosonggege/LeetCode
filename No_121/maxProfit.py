c3a6062841e58f5c1670b6591f11ca073a750448
xiaosonggege <1243049371@qq.com>
2020-06-17 23:04:31 +0800

    y

--- No_121/maxProfit.py
+++ No_121/maxProfit.py
@@ -9,7 +9,7 @@
 @time: 2020/3/15 11:48 上午
 '''
 
-class maxProfit:
+class MaxProfit:
     def __init__(self, *array):
         self._array = list(array)
         self._min = self._array.pop(0)
@@ -18,6 +18,7 @@ class maxProfit:
         self._max_pre = self._min
         self._mp = 0 #最大收益
         self.calc = lambda min, max: max - min
+
 
     def __enter__(self):
         for i in self:
@@ -49,14 +50,46 @@ class maxProfit:
             self._min = value
             self._max = value
 
+class Maxprofit2:
+    def __init__(self, nums:list):
+        self._nums = nums
+        self._diff = [self._nums[i]-self._nums[i-1] for i in range(1, len(self._nums))]
+        self._result = 0
+    def maxprofit_fun2_sub(self, left, right)->int:
+        mid = (left + right) // 2
+        left_sum = 0
+        right_sum = 0
+        sum = self._diff[mid]
+        for i in range(mid-1, left-1, -1):
+            sum += self._diff[i]
+            left_sum = max(left_sum, sum)
+        sum = 0
+        for i in range(mid+1, right+1):
+            sum += self._diff[i]
+            right_sum = max(right_sum, sum)
+        return left_sum + right_sum
+
+    def maxprofit_fun2(self, left, right): #O(nlogn)
+        if left == right:
+            return self.maxprofit_fun2_sub(left=left, right=right)
+        else:
+            mid = (left + right) // 2
+            left_sum = self.maxprofit_fun2(left=left, right=max(left, mid-1))
+            right_sum = self.maxprofit_fun2(left=min(mid+1, right), right=right)
+            mid_sum = self.maxprofit_fun2_sub(left=left, right=right)
+            return max(left_sum, mid_sum, right_sum)
 
 if __name__ == '__main__':
-    with maxProfit(7, 1, 5, 3, 6, 4) as m:
-        print(m)
+    # with MaxProfit(7, 1, 5, 3, 6, 4) as m:
+    #     print(m)
+    #
+    # with MaxProfit(7, 6, 4, 3, 1) as m:
+    #     print(m)
+    #
+    # with MaxProfit(7, 3, 5, 8, 1, 4, 11) as m:
+    #     print(m)
 
-    with maxProfit(7, 6, 4, 3, 1) as m:
-        print(m)
-
-    with maxProfit(7, 3, 5, 8, 1, 4, 11) as m:
-        print(m)
+    m = Maxprofit2(nums=[7, 1, 5, 3, 6, 4])
+    result = m.maxprofit_fun2(left=0, right=4)
+    print(result)
 
